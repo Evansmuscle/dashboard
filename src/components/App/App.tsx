1d60348d95bf1dc1904062247a32b56b5f3f24cc
Evansmuscle <katokorkmaz@gmail.com>
2021-06-22 20:01:47 +0300

    made ready to code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,8 @@
-import "./App.css";
+import './App.css';
 import React from 'react';
 
 const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
-}
+  return <div>App</div>;
+};
 
 export default App;
---
+++ src/components/Topbar/Topbar.tsx
@@ -0,0 +1,8 @@
+import './Topbar.css';
+import React from 'react';
+
+const Topbar = () => {
+  return <div>Topbar</div>;
+};
+
+export default Topbar;
