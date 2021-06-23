b317bdb6b82720eaa1d26b58a05af8eb42cbea18
Evansmuscle <katokorkmaz@gmail.com>
2021-06-23 17:10:11 +0300

    finished topbar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
 import React from 'react';
 
+import Topbar from '../Topbar';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,8 +1,35 @@
 import './Topbar.css';
 import React from 'react';
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">kaanadmin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
+            alt="profile-avatar"
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
